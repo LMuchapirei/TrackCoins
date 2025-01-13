0139a8ab0d390c6291007a16276d63b57a5db674
LMuchapirei <muchapireil@gmail.com>
2025-01-13 15:44:39 +0200

    Refactor the view to use HStack with conditionals applied to modifiers

---
+++ TrackCoins/Core/Components/StatisticView.swift
@@ -0,0 +1,18 @@
+//
+//  StatisticView.swift
+//  TrackCoins
+//
+//  Created by Linval Muchapirei on 13/1/2025.
+//
+
+import SwiftUI
+
+struct StatisticView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatisticView()
+}
