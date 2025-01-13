304dc9455cc7d19faf3e43d5fefc52c30e563d3f
LMuchapirei <muchapireil@gmail.com>
2025-01-13 10:50:26 +0200

    Two services to get the coins and the image for each coin saving them in caches folder by id

---
+++ TrackCoins/Services/CoinImageService.swift
@@ -0,0 +1,48 @@
+//
+//  CoinImageService.swift
+//  CrytpoSwift
+//
+//  Created by Linval Muchapirei on 11/1/2025.
+//
+
+import Foundation
+import Combine
+import SwiftUI
+
+class CoinImageService {
+    @Published var image: UIImage? = nil
+    var imageSubscription : AnyCancellable?
+    private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName: String
+    init(coin: CoinModel){
+        self.coin = coin
+        self.imageName = coin.id
+        getCoinImage()
+    }
+    
+    private func getCoinImage(){
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+            print("Retrieved image from file manager")
+        } else {
+            downloadCoinImage()
+            print("Downloading image now")
+        }
+    }
+    private func downloadCoinImage(){
+        guard let url = URL(string: coin.image) else { return
+        }
+        imageSubscription = NetworkingManager.download(url: url)
+                            .tryMap({(data) -> UIImage? in
+                                return UIImage(data: data)
+                            })
+                            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
+                                guard let self = self,let downloadedImage = returnedImage else { return } /// guard on the returnedImage fixed an error thrown on the return type of out tryMap call above
+                                self.image = downloadedImage
+                                self.imageSubscription?.cancel()
+                                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
+        })
+    }
+}
